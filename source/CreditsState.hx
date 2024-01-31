e4180dc18b4486cfa65a986b061f4cccb8077a49
DafsThirdAltAccount <158230366+DafsThirdAltAccount@users.noreply.github.com>
2024-01-30 21:47:09 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -100,7 +100,10 @@ class CreditsState extends MusicBeatState
 			['ninjamuffin99',		'ninjamuffin99',	"Programmer of Friday Night Funkin'",						'https://twitter.com/ninja_muffin99',	'F73838'],
 			['PhantomArcade',		'phantomarcade',	"Animator of Friday Night Funkin'",							'https://twitter.com/PhantomArcade3K',	'FFBB1B'],
 			['evilsk8r',			'evilsk8r',			"Artist of Friday Night Funkin'",							'https://twitter.com/evilsk8r',			'53E52C'],
-			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",							'https://twitter.com/kawaisprite',		'6475F3']
+			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",							'https://twitter.com/kawaisprite',		'6475F3'],
+			[''],
+			["Modpack made by"],
+			['DaffyToons',		'daffytoons',	"2 Years of Mod Collecting, Worth it",						'https://www.instagram.com/daffytoons.arts',	'0A8451']
 		];
 		
 		for(i in pisspoop){
